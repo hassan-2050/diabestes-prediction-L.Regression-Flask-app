097d623754b010734c8b9a00596481284f5a3a72
hassan2050 <syedhassan2050@gmail.com>
2024-02-09 17:15:43 +0530

    commit

--- app.py
+++ app.py
@@ -1,10 +1,54 @@
-from flask import Flask
+from flask import Flask,render_template,request
+import pickle
 
-app = Flask(__name__)
+
+scaler=pickle.load(open("Model/standardScalar.pkl","rb"))
+model=pickle.load(open("Model/modelForPrediction.pkl","rb"))
+app=Flask(__name__)
+
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+
+def hello():
+    return render_template('index.html')
+    
+
+
+@app.route("/predict_datapoint",methods=["GET","POST"])
+def predict_datapoint():
+      if request.method=="POST":
+            preg=int(request.form.get("Pregnancies"))
+            glu=int(request.form.get("Glucose"))
+            bp=int(request.form.get("BloodPressure"))
+            skin=int(request.form.get("SkinThickness"))
+            ins=int(request.form.get("Insulin"))
+            bmi=int(request.form.get("BMI"))
+            pd=int(request.form.get("DiabetesPedigreeFunction"))
+            age=int(request.form.get("Age"))
+            lists=[preg,glu,bp,skin,ins,bmi,pd,age]
+            scaled=scaler.transform([lists])
+            output=model.predict(scaled)
+            if output[0]==1:
+                  final="Diabetic"
+                  page="single_prediction.html"
+            else:
+                  final="Non Diabetic"
+                  page="single_prediction2.html"      
+            return render_template(page,result=final)
+
+
+
+
+
+
+      else:
+           return render_template('home.html')
+
+            
+      
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
+
+
+
